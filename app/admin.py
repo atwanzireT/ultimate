757035e280fe1e43d96b930ac94310a2e3cde336
atwanzireT <tatwanzire@gmail.com>
2022-07-03 15:16:04 +0300

    created models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from app.models import *
+
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(District)
+admin.site.register(Village)
+admin.site.register(Plot)
+admin.site.register(Plot_Owner)
+admin.site.register(Registration)
+admin.site.register(Feedback)
+admin.site.register(Appointments)
+admin.site.register(Updates)
+
--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from distutils.command.upload import upload
+from email.mime import image
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -33,5 +36,53 @@ class Plot(models.Model):
         return self.plot_number
 
 # registration
+class Plot_Owner(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user
+
 class Registration(models.Model):
-    pass
+    plot = models.ForeignKey(Plot, on_delete=models.CASCADE)
+    secret_code = models.UUIDField(unique=True)
+    owner = models.ForeignKey(Plot_Owner, on_delete=models.CASCADE)
+    registedBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    agreement_document = models.FileField(help_text="Agreement to the plot of Land Scanned")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.plot
+     
+class Feedback(models.Model):
+    title = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    body = models.TextField()
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Appointments(models.Model):
+    title = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    detail = models.TextField()
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Updates(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "updates")
+    detail = models.TextField()
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
